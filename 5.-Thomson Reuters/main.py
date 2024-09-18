b9cc2c4805d268fda4d97786ca2f9e0a2d313d75
“di_cer0” <dcervantesr1401@alumno.ipn.mx>
2024-09-18 17:37:42 -0600

    MechaBible Mark I: Programming - Python: Artificial Intelligence FastAPI usage

--- 5.-Thomson Reuters/main.py
+++ 5.-Thomson Reuters/main.py
@@ -1,35 +1,37 @@
-from fastapi import FastAPI, HTTPException
-from sse_starlette import EventSourceResponse
+# -*- coding: utf-8 -*-
+
+import fastapi
+import sse_starlette
+
 from modules.database_models import DatabaseModels
 from modules.llm import GorpAssistant
 from API_Keys.Llaves_ChatGPT_Bard_etc import ChatGPT_Key, DB_Password
 
+app = fastapi.FastAPI()
 
-app = FastAPI()
-
-
-db = DatabaseModels(dbname = 'c1_AI_ChatHistory', user = 'postgres', password = DB_Password)
-llm_assistant = GorpAssistant(api_key = ChatGPT_Key)
-
+db = DatabaseModels(dbname='c1_AI_ChatHistory', user='postgres', password=DB_Password)
+llm_assistant = GorpAssistant(api_key=ChatGPT_Key)
 
 @app.on_event("shutdown")
 def shutdown_event():
     db.close()
 
+@app.get("/")
+def welcome_chat():
+    return "Welcome to the Gorp Assistant Chat"
 
 @app.post("/chat/new")
 def create_chat():
     chat_session_id = db.create_chat_session()
     return {"chat_session_id": chat_session_id}
 
-
 @app.post("/chat/{session_id}/send")
 async def send_message(session_id: int, user_message: str):
     history = db.get_chat_history(session_id)
 
     if history is None:
-        raise HTTPException(status_code=404, detail="Chat session not found")
-    
+        raise fastapi.HTTPException(status_code=404, detail="Chat session not found")
+
     db.add_message(user_message, "user", session_id)
     stream = await llm_assistant.prompt_llm_async(user_message)
 
@@ -37,16 +39,15 @@ async def send_message(session_id: int, user_message: str):
         async for chunk in stream:
             content = chunk["choices"][0]["delta"]["content"]
             yield content
-    
+
     assistant_message = ''.join([chunk async for chunk in stream_response()])
     db.add_message(assistant_message, "assistant", session_id)
 
-    return EventSourceResponse(stream_response())
-
+    return sse_starlette.EventSourceResponse(stream_response())
 
 @app.get("/chat/{session_id}/history")
 def get_chat_history(session_id: int):
     history = db.get_chat_history(session_id)
     if not history:
-        raise HTTPException(status_code=404, detail="No messages found for this session")
+        raise fastapi.HTTPException(status_code=404, detail="No messages found for this session")
     return history
